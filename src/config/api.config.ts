7c8f5f67328d4dd4d94aa291e1128d20b665b779
Gustavo Alves <gustavo_alves@outlook.com>
2018-03-04 17:51:18 -0300

    Componente Loading na tela de detalhamento de produto

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    //baseUrl: 'http://localhost:8080',
-    baseUrl: 'https://udemy-curso-spring-ionic.herokuapp.com/',
+    baseUrl: 'http://localhost:8080',
+    //baseUrl: 'https://udemy-curso-spring-ionic.herokuapp.com/',
     bucketBaseUrl: 'https://s3-sa-east-1.amazonaws.com/udemy-curso-spring-ionic'
 }
